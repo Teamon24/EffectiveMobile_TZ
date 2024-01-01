a005c3a9dec8a2c3fb01ea9ea1b35b05b6f6641f
Artem.Leshchenko <teamon24@gmail.com>
2024-01-02 02:59:08 +0400

    build.gradle refactoring

--- deploy/api/_functions.sh
+++ deploy/api/_functions.sh
@@ -1,22 +1,34 @@
-function printProps() {
-  SCRIPS_NAME=$1
-  PROPS=$2
-  echo "$SCRIPS_NAME"
-  echo "properties = $PROPS"
+printTitle() {
+    N=${2:-80}
+    TITLE=$1
+    EMPTY="$(repeat $(expr length "$1"))"
+    PART="$(repeat $N)"
+
+    echo "$PART $EMPTY $PART"
+    echo "$PART $TITLE $PART"
+    echo "$PART $EMPTY $PART"
 }
 
-function printJavaHome() {
+repeat(){
+    local start=1
+    local end=${1:-80}
+    local str="${2:-=}"
+    local range=$(seq $start $end)
+    for i in $range ; do echo -n "${str}"; done
+}
+
+printJavaHome() {
     TITLE=${1:-""}
     echo "============================== $TITLE ====================================="
     echo "JAVA_HOME=$JAVA_HOME"
     echo "PATH=$PATH"
 }
 
-function existsInList() {
+existsInList() {
     LIST=$1
     DELIMITER=$2
     VALUE=$3
-    LIST_WHITESPACES=`echo $LIST | tr "$DELIMITER" " "`
+    LIST_WHITESPACES=$(split "$LIST" "$DELIMITER")
     for x in $LIST_WHITESPACES; do
         if [ "$x" = "$VALUE" ]; then
             return 0
@@ -25,7 +37,22 @@ function existsInList() {
     return 1
 }
 
-function isANumber() {
+printSplit() {
+    LIST=$1
+    for x in $LIST; do
+        if [ "$x" = "$VALUE" ]; then
+            echo x
+        fi
+    done
+}
+
+split() {
+    STRING=$1
+    DELIMITER=$2
+    echo $STRING | tr "$DELIMITER" " "
+}
+
+isANumber() {
     ARG_NAME=$2
     ARG=$1
     re='^[0-9]+$'
